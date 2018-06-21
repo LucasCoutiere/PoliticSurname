dcfe2ba344e77dd670b3e3512188e4a0e32c7c46
Lucas Coutière <lucascoutiere@gmail.com>
2018-06-21 13:58:27 +0200

    Delete correct.js

--- js/correct.js
+++
@@ -1,8 +0,0 @@
-var textNode,
-  walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false)
-while ((textNode = walk.nextNode())) {
-  textNode.nodeValue = textNode.nodeValue
-    .replace(/Hollande/gi, 'Flamby')
-    .replace(/François Hollande/gi, 'Flamby')
-    .replace(/d'Hollande/gi, 'de Flamby')
-}
